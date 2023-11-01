6eb6ba318eb11722555e975d5d4a2751116f8256
arushisaranath <arushisaranath@gmail.com>
2023-11-01 04:59:34 -0400

    Added docstrings for the is_prime function

--- prime.py
+++ prime.py
@@ -7,6 +7,18 @@
 import math
 
 def is_prime(n):
+    """
+    To check if a number is prime or not.
+
+    Its inputs are integers and outputs are 'True' or 'False' depending on whether the input integer is prime or not respectively. 
+    
+    Example:
+        1) is_prime(7)
+        True
+
+        2) is_prime(9)
+        False
+    """
     if n <= 1:
         return False
     for i in range(2, int(math.sqrt(n)) + 1):
