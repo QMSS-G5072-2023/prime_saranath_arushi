ea3cf8760d3721e29db71565e5548cf6907f45bb
arushisaranath <arushisaranath@gmail.com>
2023-11-01 21:22:21 -0400

    init setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="prime_as6723",
+    version="0.1.0",
+    packages=find_packages(),
+)
+
